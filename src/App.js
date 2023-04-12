0d8852311b928bb70595e726248e2dc93fbd4d28
Patro <binod.patro@accenture.com>
2023-04-12 10:20:16 +0530

    solutionformodule1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import Quiz from './Quiz.js'
+
 class App extends Component {
   render() {
     return (
-      <div />
+      //<div />
+      <Quiz />
     )
   }
 }
-
-export default App
